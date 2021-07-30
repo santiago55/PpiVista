00a24d2bb14f8f778b52d1da30c4789f454e29cb
santiago966 <santyboal2@gmail.com>
2021-07-30 17:29:02 -0500

    Ahorros

--- src/pages/Ahorros.js
+++ src/pages/Ahorros.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import ListaAhorros from './ListaAhorros'
 
-function Ahorros({ahorros, guardarEjecutar}) {
-   
+function Ahorros({ ahorros, guardarEjecutar }) {
+
     return (
+
         <div className="container p-4">
             <div className="row">
-                {ahorros.map(ahorro => (
-                    <ListaAhorros
-                        key={ahorro._id}
-                        guardarEjecutar={guardarEjecutar}
-                        ahorro={ahorro}
-                    />
-                ))}
+                <div className="col-md-4 p-2">
+                    <div>
+                        <table id="tabla" className="table">
+                            <thead>
+                                <tr>
+                                    <th checkboxSelection></th>
+                                    <th>Descripción</th>
+                                    <th>Cantidad</th>
+                                    <th>Tipo</th>
+                                    <th>fecha</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {ahorros.map(ahorro1 => (
+                                    <ListaAhorros
+                                        key={ahorro1._id}
+                                        guardarEjecutar={guardarEjecutar}
+                                        ahorro1={ahorro1}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
